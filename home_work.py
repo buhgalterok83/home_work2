8172ede4ba3799a07a4e490508bbc4ac243deca0
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-04-24 23:25:30 +0300

    3

--- home_work.py
+++ home_work.py
@@ -1,18 +1,23 @@
-lst = []
 def second_largest_number(lst):
     if len(lst) < 2 or all(elem == lst[0] for elem in lst):
         return None
-    largest = float('-inf')
+    largest = second_largest = float('-inf')
     for num in lst:
         if num > largest:
+            second_largest = largest
             largest = num
-    second_largest = float('-inf')
-    for num in lst:
-        if num != largest and num > second_largest:
+        elif num > second_largest and num != largest:
             second_largest = num
-    if lst.count(largest) > 1 and second_largest != float('-inf'):
-        return second_largest
-    else:
-        return largest if second_largest == float('-inf') else second_largest
 
-print(second_largest_number(lst))
+    return second_largest if second_largest != float('-inf') else largest
+
+
+lst1 = [4, 2, 1, 5, 2, 5, 7]
+lst2 = [4, 4]
+lst3 = [0, 0, 0]
+lst4 = [4, 2, 1, 5, 2, 5, 7, 10, 10, 1]
+
+print(second_largest_number(lst1))  # 5
+print(second_largest_number(lst2))  # None
+print(second_largest_number(lst3))  # None
+print(second_largest_number(lst4))  # 7
