c1408cd941b17494a3144561c026cb23b8e6e294
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-05-24 14:10:09 +0300

    Update home_work38.py

--- home_work3.py
+++ home_work3.py
@@ -1,8 +1,25 @@
-def read_last(file_path, symbol_number):
-    with open(file_path, 'r') as file:
-        for line in file:
-            line = line.rstrip('\n')
-            if line:
-                last_symbols = line[-symbol_number:]
-                print(last_symbols)
-read_last('/Users/goncharovvitalii/Downloads/read_last.txt', 6)
+import functools
+
+def cache_result(func):
+    cache = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        key = (args, frozenset(kwargs.items()))
+        if key in cache:
+            return cache[key]
+        result = func(*args, **kwargs)
+        cache[key] = result
+        return result
+
+    return wrapper
+@cache_result
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(5))  # 5 (вычисление)
+print(fibonacci(5))  # 5 (из кэша)
+print(fibonacci(10))  # 55 (вычисление)
+print(fibonacci(10))  # 55 (из кэша)
