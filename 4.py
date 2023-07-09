21912433d788759f53259f98f0639cae9aad2a74
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-07-09 18:06:10 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,14 +1,27 @@
+import xml.etree.ElementTree as ET
 import json
-import xmltodict
 
 def convert_xml_to_json(xml_file, json_file):
-    with open(xml_file, 'r') as f:
-        xml_data = f.read()
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
 
-    json_data = json.dumps(xmltodict.parse(xml_data), indent=4)
+    result = {}
+    for page in root.findall('page'):
+        page_name = page.get('name')
+        elements = {}
+        for element in page.findall('element'):
+            element_name = element.get('name')
+            locators = {}
+            for locator in element.findall('locator'):
+                platform = locator.get('platform')
+                locator_type = locator.get('locator_type')
+                locator_value = locator.text
+                locators[platform] = [locator_type, locator_value]
+            elements[element_name] = locators
+        result[page_name] = elements
 
     with open(json_file, 'w') as f:
-        f.write(json_data)
+        json.dump(result, f, indent=4)
 
 # Пример использования
-convert_xml_to_json('input.xml', 'output.json')
+convert_xml_to_json('/Users/vitaliigoncharov/Desktop/pages.xml', '/Users/vitaliigoncharov/Desktop/pages.json')
